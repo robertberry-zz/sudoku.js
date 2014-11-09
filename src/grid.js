380100df81d52d54015a0a05dd9d74dbacdc2d0e
Robert Berry <rjberry@gmail.com>
2014-11-09 22:17:20 +0000

    Show numbers

--- src/grid.js
+++ src/grid.js
@@ -3,7 +3,9 @@ var Immutable = require('immutable');
 var React = require('react/addons');
 
 var CELL_SIZE = 30,
-    BORDER_SIZE = 1;
+    BORDER_SIZE = 1,
+    TEXT_X_OFFSET = 7,
+    TEXT_Y_OFFSET = 25;
 
 function position(a) {
     return (Math.floor(a / 3) + 2) * BORDER_SIZE +
@@ -13,16 +15,24 @@ function position(a) {
 var Cell = component(function (props) {
     var value = props.cursor.get('value');
 
-    return React.DOM.rect({
-        x: position(props.x),
-        y: position(props.y),
-        width: CELL_SIZE,
-        height: CELL_SIZE,
-        className: React.addons.classSet({
-            "sudoku__cell": true,
-            "sudoku__cell--editable": props.cursor.get('editable')
-        })
-    });
+    return React.DOM.g({}, 
+        React.DOM.rect({
+            x: position(props.x),
+            y: position(props.y),
+            width: CELL_SIZE,
+            height: CELL_SIZE,
+            className: React.addons.classSet({
+                "sudoku__cell": true,
+                "sudoku__cell--editable": props.cursor.get('editable')
+            })
+        }), value ? [
+            React.DOM.text({
+                x: position(props.x) + TEXT_X_OFFSET,
+                y: position(props.y) + TEXT_Y_OFFSET,
+                className: "sudoku__cell-text"
+            }, value)
+        ] : []
+    );
 });
 
 var Grid = component(function (props) {
